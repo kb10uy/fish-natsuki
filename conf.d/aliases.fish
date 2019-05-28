7e9feafe9fcba5ae70f68b5b238a9d87e9f9036d
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-05-28 15:57:02 +0900

    initial commit

---
+++ conf.d/aliases.fish
@@ -0,0 +1,8 @@
+alias d='docker'
+alias dc='docker-compose'
+
+alias dcu='docker-compose up -d'
+alias dcd='docker-compose down'
+alias dce='docker-compose exec -u 1000'
+alias dcer='docker-compose exec -u 0'
+alias dcb='docker-compose build'
